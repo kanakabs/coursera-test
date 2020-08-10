21e748a8d5e671cedbc8f244c439f987a3d701cf
kanakabs <45517909+kanakabs@users.noreply.github.com>
2020-08-10 22:53:03 +0530

    Create script.js

---
+++ module4-solution/script.js
@@ -0,0 +1,17 @@
+var names=["python", "Java", "ruby", "jquery", "Dart", "Javascript"];
+
+for (var i=0;i<names.length;i++) 
+{
+	var chara = names[i].charAt(0);
+	if(chara == "j" || chara == "J"){
+
+		console.log("GOODBYE.... "+names[i]);
+
+	}
+	else{
+		console.log("HELLO.... "+names[i]);
+	}
+	
+	
+
+}
